7c842612e02789bf72a89012686538d01fe6a20f
isbeauty <nooridayuidrus@gmail.com>
2021-02-07 03:23:02 +0800

    modified
    appjs and css

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
     navigator.serviceWorker.register("/service-worker.js");
   });
   document.addEventListener("DOMContentLoaded", function() {
-    pjax = new Pjax({selectors:["title", ".app-inner", ".pjax-header"], cacheBust:!1, switches:{".app-inner":Pjax.switches.sideBySide}, switchesOptions:{".app-inner":{classNames:{remove:"Animated Animated--reverse Animate--fast Animate--noDelay", add:"Animated", backward:"Animate--slideInRight", forward:"Animate--slideInLeft"}, callbacks:{removeElement:function(a) {
+    pjax = new Pjax({selectors:["title", ".app-inner", ".pjax-header"], cacheBust:!1, switches:{".app-inner":Pjax.switches.sideBySide}, switchesOptions:{".app-inner":{classNames:{remove:"animate__animated Animated--reverse animate__faster Animate--noDelay", add:"animate__animated", backward:"animate__fadeInRight", forward:"animate__fadeInLeft"}, callbacks:{removeElement:function(a) {
       a.style.marginLeft = "-" + a.getBoundingClientRect().width / 2 + "px";
     }}}}});
   });
