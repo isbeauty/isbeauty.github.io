fb312d0ba6dd7f7f87aafbfa874e3933ba8dfc01
isbeauty <nooridayuidrus@gmail.com>
2021-02-08 01:21:03 +0800

    manifest & SW
    editted

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 "serviceWorker" in navigator && window.addEventListener("load", function() {
-    navigator.serviceWorker.register("/service-worker.js");
+  navigator.serviceWorker.register("/service-worker.js");
+});
+document.addEventListener("DOMContentLoaded", function() {
+  pjax = new Pjax({
+      selectors: ["title", ".app-inner", ".pjax-header"],
+      cacheBust: !1
   });
-  document.addEventListener("DOMContentLoaded", function() {
-    pjax = new Pjax({selectors:["title", ".app-inner", ".pjax-header"], cacheBust:!1, switches:{".app-inner":Pjax.switches.sideBySide}, switchesOptions:{".app-inner":{classNames:{remove:"animate__animated Animated--reverse animate__faster Animate--noDelay", add:"animate__animated", backward:"animate__fadeInRight", forward:"animate__fadeInLeft"}, callbacks:{removeElement:function(a) {
-      a.style.marginLeft = "-" + a.getBoundingClientRect().width / 2 + "px";
-    }}}}});
-  });
+});
--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,44 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.0.2/workbox-sw.js');
-const {registerRoute} = workbox.routing;
-const {CacheFirst} = workbox.strategies;
+const {registerRoute, setDefaultHandler, setCatchHandler} = workbox.routing;
+const {CacheFirst,NetworkOnly} = workbox.strategies;
 const {CacheableResponse,CacheableResponsePlugin} = workbox.cacheableResponse;
 const {ExpirationPlugin} = workbox.expiration;
 const {skipWaiting, clientsClaim} = workbox.core;
+const {matchPrecache, precacheAndRoute} = workbox.precaching;
 
-const cdnCacheName = 'cdnProvider';
+const CacheName = 'sWstore';
 const maxAgeSeconds = 60 * 60 * 24 * 365;
-
+const FALLBACK_HTML_URL = '/offline/';
 self.skipWaiting();
 clientsClaim();
 
+precacheAndRoute([
+  {url: '/offline/', revision: null },
+  {url: '/manifest.json', revision: 210208},
+  {url: '/favicon.ico', revision: 210208},
+  {url: '/images/logo/icon-192x192.png', revision: 210208},
+  {url: '/images/logo/icon-256x256.png', revision: 210208},
+  {url: '/images/logo/icon-384x384.png', revision: 210208},
+  {url: '/images/logo/icon-512x512.png', revision: 210208},
+  {url: '/images/logo/maskable_icon_x512.png', revision: 210208},
+]);
+
+setDefaultHandler(new NetworkOnly());
+
+setCatchHandler(async ({ event }) => {
+  // Return the precached offline page if a document is being requested
+  if (event.request.destination === 'document') {
+    return matchPrecache('/offline/');
+  }
+
+  return Response.error();
+});
+
 registerRoute(
-  ({ url }) => url.origin === 'https://cdn.jsdelivr.net',
+  ({ url }) => url.origin === 'https://cdn.jsdelivr.net' ||
+               url.origin === 'https://cdnjs.cloudflare.com',
   new CacheFirst({
-    cacheName: cdnCacheName,
+    cacheName: CacheName,
     plugins: [
       new CacheableResponsePlugin({
         statuses: [0, 200],
@@ -29,8 +53,12 @@ registerRoute(
 registerRoute(
   ({request}) => request.destination === 'image',
   new CacheFirst({
+    cacheName: CacheName,
     plugins: [
-      new CacheableResponsePlugin({statuses: [0, 200]})
+      new CacheableResponsePlugin({statuses: [0, 200]}),
+      new ExpirationPlugin({
+        maxAgeSeconds,purgeOnQuotaError: true,
+      })
     ],
   })
 );
