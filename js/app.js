5afdc4f7d960409eedd453ff3c2c2177c5ffca9e
isbeauty <nooridayuidrus@gmail.com>
2021-02-09 03:11:44 +0800

    updated content

--- js/app.js
+++
@@ -1,9 +0,0 @@
-"serviceWorker" in navigator && window.addEventListener("load", function() {
-  navigator.serviceWorker.register("/service-worker.js");
-});
-document.addEventListener("DOMContentLoaded", function() {
-  pjax = new Pjax({
-      selectors: ["title", ".app-inner", ".pjax-header"],
-      cacheBust: !1
-  });
-});
